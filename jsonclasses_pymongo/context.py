effe98d2138f233fa884e17256646f42c4aa8955
Victor Zhang <yeannylam@gmail.com>
2020-10-04 14:34:41 +0800

    rewrite list and dict with new context

---
+++ jsonclasses_pymongo/context.py
@@ -0,0 +1,39 @@
+"""This module defines encoding and decoding context objects."""
+from typing import NamedTuple, Any, List, Dict, Union, TYPE_CHECKING
+from jsonclasses import Types, LookupMap
+if TYPE_CHECKING:
+    from .mongo_object import MongoObject
+
+
+class EncodingContext(NamedTuple):
+    """Encoding context contains necessary information for encoding JSON Class
+    objects into database writing commands.
+    """
+    value: Any
+    types: Types
+    keypath_root: str
+    root: MongoObject
+    keypath_owner: str
+    owner: MongoObject
+    keypath_parent: str
+    parent: Union[List[Any], Dict[str, Any], MongoObject]
+    lookup_map: LookupMap
+
+    def new(self, **kwargs):
+        """Return a new encoding context by replacing provided values."""
+        keys = kwargs.keys()
+        return EncodingContext(
+            value=kwargs['value'] if 'value' in keys else self.value,
+            types=kwargs['types'] if 'types' in keys else self.types,
+            keypath_root=(kwargs['keypath_root']
+                          if 'keypath_root' in keys else self.keypath_root),
+            root=kwargs['root'] if 'root' in keys else self.root,
+            keypath_owner=(kwargs['keypath_owner']
+                           if 'keypath_owner' in keys else self.keypath_owner),
+            owner=kwargs['owner'] if 'owner' in keys else self.owner,
+            keypath_parent=(kwargs['keypath_parent']
+                            if 'keypath_parent' in keys
+                            else self.keypath_parent),
+            parent=kwargs['parent'] if 'parent' in keys else self.parent,
+            lookup_map=(kwargs['lookup_map']
+                        if 'lookup_map' in keys else self.lookup_map))
--- jsonclasses_pymongo/encoder.py
+++ jsonclasses_pymongo/encoder.py
@@ -1,90 +1,80 @@
 from __future__ import annotations
-from typing import (List, Dict, Any, Optional, TypeVar, Tuple, cast,
-                    TYPE_CHECKING)
-from jsonclasses import (fields, Types, Config, FieldType, FieldStorage,
-                         resolve_types)
+from typing import (List, Dict, Any, NamedTuple, Optional, TypeVar, Tuple,
+                    cast, TYPE_CHECKING)
+from jsonclasses import (fields, types, Config, FieldType, FieldStorage,
+                         resolve_types, LookupMap, concat_keypath)
 from datetime import datetime
 from inflection import camelize
 from bson.objectid import ObjectId
 from .coder import Coder
 from .utils import (ref_field_key, ref_field_keys, ref_db_field_key,
                     ref_db_field_keys)
+from .context import EncodingContext
 from .write_command import WriteCommand
-
 if TYPE_CHECKING:
     from .mongo_object import MongoObject
     T = TypeVar('T', bound=MongoObject)
 
 
+class EncodingResult(NamedTuple):
+    """The result from encoding an item."""
+    result: Any
+    write_commands: List[WriteCommand]
+
+
 class Encoder(Coder):
+    """Write commands encoder."""
 
-    def encode_list(
-        self,
-        value: Optional[List[Any]],
-        owner: T,
-        types: Any,
-        parent: Optional[T] = None,
-        parent_linkedby: Optional[str] = None
-    ) -> Tuple[Optional[List[Any]], List[WriteCommand]]:
-        if value is None:
-            return None, []
-        item_types = resolve_types(
-            types.field_description.list_item_types)
-        if types.field_description.field_storage == FieldStorage.FOREIGN_KEY:
-            item_types = item_types.linkedby(
-                types.field_description.foreign_key)
-        elif types.field_description.field_storage == FieldStorage.LOCAL_KEY:
+    def encode_list(self, context: EncodingContext) -> EncodingResult:
+        if context.value is None:
+            return EncodingResult(result=None, write_commands=[])
+        value = cast(List[Any], context.value)
+        fd = context.types.field_description
+        item_types = resolve_types(fd.list_item_types)
+        if fd.field_storage == FieldStorage.FOREIGN_KEY:
+            fk = cast(str, fd.foreign_key)
+            item_types = item_types.linkedby(fk)
+        if fd.field_storage == FieldStorage.LOCAL_KEY:
             item_types = item_types.linkto
-        else:
-            pass
-        dest = []
+        result = []
         write_commands = []
-        for item in value:
-            new_value, commands = self.encode_item(
+        for index, item in enumerate(value):
+            item_result, item_commands = self.encode_item(context.new(
                 value=item,
-                owner=owner,
                 types=item_types,
-                parent=parent,
-                parent_linkedby=parent_linkedby
-            )
-            if (types.field_description.field_storage ==
-                    FieldStorage.FOREIGN_KEY):
-                dest.append(new_value)
-            elif (types.field_description.field_storage ==
-                    FieldStorage.LOCAL_KEY):
-                dest.append(new_value['_id'])
+                keypath_root=concat_keypath(context.keypath_root, index),
+                keypath_owner=concat_keypath(context.keypath_owner, index),
+                keypath_parent=str(index),
+                parent=value))
+            if fd.field_storage == FieldStorage.FOREIGN_KEY:
+                result.append(item_result)
+            elif fd.field_storage == FieldStorage.LOCAL_KEY:
+                result.append(item_result['_id'])
             else:
-                dest.append(new_value)
-            write_commands.extend(commands)
-        return dest, write_commands
+                result.append(item_result)
+            write_commands.extend(item_commands)
+        return EncodingResult(result=result, write_commands=write_commands)
 
-    def encode_dict(
-        self,
-        value: Optional[Dict[str, Any]],
-        owner: T,
-        types: Any,
-        parent: Optional[T] = None,
-        parent_linkedby: Optional[str] = None
-    ) -> Tuple[Optional[Dict[str, Any]], List[WriteCommand]]:
-        if value is None:
-            return None, []
-        config: Config = owner.__class__.config
-        item_types = resolve_types(
-            types.field_description.dict_item_types)
-        dest = {}
+    def encode_dict(self, context: EncodingContext) -> EncodingResult:
+        if context.value is None:
+            return EncodingResult(result=None, write_commands=[])
+        value = cast(Dict[str, Any], context.value)
+        fd = context.types.field_description
+        item_types = resolve_types(fd.dict_item_types)
+        camelized = context.owner.__class__.config.camelize_db_keys
+        result = {}
         write_commands = []
-        for (key, item) in value.items():
-            new_value, commands = self.encode_item(
+        for key, item in value.items():
+            item_result, item_commands = self.encode_item(context.new(
                 value=item,
-                owner=owner,
                 types=item_types,
-                parent=parent,
-                parent_linkedby=parent_linkedby
-            )
-            dest[camelize(key, False)
-                 if config.camelize_db_keys else key] = new_value
-            write_commands.extend(commands)
-        return dest, write_commands
+                keypath_root=concat_keypath(context.keypath_root, key),
+                keypath_owner=concat_keypath(context.keypath_owner, key),
+                keypath_parent=str(key),
+                parent=value))
+            result[camelize(key, False) if camelized else key] = item_result
+            write_commands.extend(item_commands)
+        return EncodingResult(result, write_commands)
 
     def encode_shape(
         self,
@@ -244,49 +234,35 @@ class Encoder(Coder):
                 dest, value.__class__.collection()))
         return dest, write_commands
 
-    def encode_item(
-        self,
-        value: Any,
-        owner: T,
-        types: Types,
-        parent: Optional[T] = None,
-        parent_linkedby: Optional[str] = None
-    ) -> Tuple[Any, List[WriteCommand]]:
-        if value is None:
-            return (value, [])
-        if types.field_description.field_type == FieldType.DATE:
-            return datetime.fromisoformat(value.isoformat()), []
-        elif types.field_description.field_type == FieldType.LIST:
-            return self.encode_list(value=value,
-                                    owner=owner,
-                                    types=types,
-                                    parent=parent,
-                                    parent_linkedby=parent_linkedby)
-        elif types.field_description.field_type == FieldType.DICT:
-            return self.encode_dict(value=value,
-                                    owner=owner,
-                                    types=types,
-                                    parent=parent,
-                                    parent_linkedby=parent_linkedby)
-        elif types.field_description.field_type == FieldType.SHAPE:
-            return self.encode_shape(value=value,
-                                     owner=owner,
-                                     types=types,
-                                     parent=parent,
-                                     parent_linkedby=parent_linkedby)
-        elif types.field_description.field_type == FieldType.INSTANCE:
-            return self.encode_instance(value=value,
-                                        owner=owner,
-                                        parent=parent,
-                                        types=types,
-                                        parent_linkedby=parent_linkedby)
+    def encode_item(self, context: EncodingContext) -> EncodingResult:
+        if context.value is None:
+            return EncodingResult(result=None, write_commands=[])
+        field_type = context.types.field_description.field_type
+        if field_type == FieldType.LIST:
+            return self.encode_list(context)
+        elif field_type == FieldType.DICT:
+            return self.encode_dict(context)
+        elif field_type == FieldType.SHAPE:
+            return self.encode_shape(context)
+        elif field_type == FieldType.INSTANCE:
+            return self.encode_instance(context)
+        elif field_type == FieldType.DATE:
+            return EncodingResult(
+                result=datetime.fromisoformat(context.value.isoformat()),
+                write_commands=[])
         else:
-            return value, []
+            return EncodingResult(context.value, [])
 
     # return save commands
     def encode_root(self, root: T) -> List[WriteCommand]:
-        return self.encode_instance(value=root,
-                                    owner=root,
-                                    types=None,
-                                    parent=None,
-                                    parent_linkedby=None)[1]
+        return self.encode_instance(EncodingContext(
+            value=root,
+            types=types.instanceof(root.__class__),
+            keypath_root='',
+            root=root,
+            keypath_owner='',
+            owner=root,
+            keypath_parent='',
+            parent=root,
+            lookup_map=LookupMap()
+        ))[1]
