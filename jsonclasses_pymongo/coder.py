929b893b08d3b2049acdf7c8c5146667c2c67bf5
Victor Zhang <yeannylam@gmail.com>
2021-04-27 16:35:13 +0800

    new connection

--- jsonclasses_pymongo/coder.py
+++ jsonclasses_pymongo/coder.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from typing import cast, TYPE_CHECKING
-from jsonclasses import (Field, FieldType, FieldStorage, resolve_types)
+from jsonclasses.jsonclass_field import JSONClassField
+from jsonclasses.field_definition import FieldType, FieldStorage
+from jsonclasses.types_resolver import TypesResolver
 from inflection import camelize
 if TYPE_CHECKING:
     from .pymongo_object import PymongoObject
@@ -8,49 +10,50 @@ if TYPE_CHECKING:
 
 class Coder():
 
-    def is_id_field(self, field: Field) -> bool:
-        return field.fdesc.primary
+    def is_id_field(self, field: JSONClassField) -> bool:
+        return field.definition.primary
 
-    def is_instance_field(self, field: Field) -> bool:
-        return field.fdesc.field_type == FieldType.INSTANCE
+    def is_instance_field(self, field: JSONClassField) -> bool:
+        return field.definition.field_type == FieldType.INSTANCE
 
-    def is_list_field(self, field: Field) -> bool:
-        return field.fdesc.field_type == FieldType.LIST
+    def is_list_field(self, field: JSONClassField) -> bool:
+        return field.definition.field_type == FieldType.LIST
 
-    def is_foreign_key_storage(self, field: Field) -> bool:
-        field_storage = field.fdesc.field_storage
+    def is_foreign_key_storage(self, field: JSONClassField) -> bool:
+        field_storage = field.definition.field_storage
         return field_storage == FieldStorage.FOREIGN_KEY
 
-    def is_local_key_storage(self, field: Field) -> bool:
-        field_storage = field.fdesc.field_storage
+    def is_local_key_storage(self, field: JSONClassField) -> bool:
+        field_storage = field.definition.field_storage
         return field_storage == FieldStorage.LOCAL_KEY
 
-    def is_foreign_key_reference_field(self, field: Field) -> bool:
+    def is_foreign_key_reference_field(self, field: JSONClassField) -> bool:
         return (self.is_instance_field(field) and
                 self.is_foreign_key_storage(field))
 
-    def is_foreign_keys_reference_field(self, field: Field) -> bool:
+    def is_foreign_keys_reference_field(self, field: JSONClassField) -> bool:
         return (self.is_list_field(field) and
                 self.is_foreign_key_storage(field))
 
-    def is_local_key_reference_field(self, field: Field) -> bool:
+    def is_local_key_reference_field(self, field: JSONClassField) -> bool:
         return (self.is_instance_field(field) and
                 self.is_local_key_storage(field))
 
-    def is_local_keys_reference_field(self, field: Field) -> bool:
+    def is_local_keys_reference_field(self, field: JSONClassField) -> bool:
         return self.is_list_field(field) and self.is_local_key_storage(field)
 
-    def is_join_table_field(self, field: Field) -> bool:
-        return field.field_types.fdesc.use_join_table is True
+    def is_join_table_field(self, field: JSONClassField) -> bool:
+        return field.types.definition.use_join_table is True
 
     def list_instance_type(self,
-                           field: Field,
+                           field: JSONClassField,
                            sibling: type[PymongoObject]
                            ) -> type[PymongoObject]:
         from .mongo_object import MongoObject
-        fd = field.field_types.fdesc
-        item_types = resolve_types(fd.raw_item_types, sibling)
-        item_fd = item_types.fdesc
+        fd = field.types.definition
+        item_types = TypesResolver().resolve_types(fd.raw_item_types,
+                                                   sibling.definition.config)
+        item_fd = item_types.definition
         return cast(type[MongoObject], item_fd.instance_types)
 
     def join_table_name(self,
---
+++ jsonclasses_pymongo/connection.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+from typing import Callable, ClassVar, Optional
+from os import getcwd, path
+from pymongo.mongo_client import MongoClient
+from pymongo.database import Database
+from pymongo.collection import Collection
+from inflection import parameterize, camelize
+
+
+ConnectedCallback = Callable[[Collection], None]
+
+
+class Connection:
+    _graph_map: dict[str, Connection] = {}
+    _initialized_map: dict[str, bool] = {}
+
+    def __new__(cls: type[Connection], graph_name: str) -> Connection:
+        if not cls._graph_map.get(graph_name):
+            cls._graph_map[graph_name] = super(Connection, cls).__new__(cls)
+        return cls._graph_map[graph_name]
+
+    def __init__(self: Connection, graph_name: str) -> None:
+        if self.__class__._initialized_map.get(graph_name):
+            return
+        self._graph_name: str = graph_name
+        self._url: Optional[str] = None
+        self._client: Optional[MongoClient] = None
+        self._database: Optional[Database] = None
+        self._collections: dict[str, Collection] = {}
+        self._connection_callbacks: dict[str, ConnectedCallback] = {}
+        self._connected: bool = False
+        self.__class__._initialized_map[graph_name] = True
+        return None
+
+    @property
+    def graph_name(self: Connection) -> str:
+        return self._graph_name
+
+    @property
+    def url(self: Connection) -> str:
+        if self._url:
+            return self._url
+        return self._generate_default_url()
+
+    def set_url(self: Connection, url: str) -> None:
+        self._url = url
+
+    def _generate_default_url(self: Connection) -> str:
+        base = 'mongodb://localhost:27017/'
+        cwd = getcwd()
+        proj = camelize(parameterize(path.basename(cwd))).lower()
+        self._url = base + proj
+        return self._url
+
+    @property
+    def client(self: Connection) -> MongoClient:
+        if self._client is not None:
+            return self._client
+        self.connect()
+        return self._client
+
+    @property
+    def database(self: Connection) -> Database:
+        if self._database is not None:
+            return self._database
+        self.connect()
+        return self._database
+
+    def connect(self: Connection) -> None:
+        self._client = MongoClient(self.url)
+        self._database = self._client.get_database()
+        self._connected = True
+        for name, callback in self._connection_callbacks.items():
+            self._call_callback(name, callback)
+
+    def disconnect(self: Connection) -> None:
+        if self._client is not None:
+            self._client.close()
+            self._client = None
+            self._database = None
+            self._collections = {}
+            self._connected = False
+
+    @property
+    def connected(self: Collection) -> bool:
+        return self._connected
+
+    def collection(self: Connection, name: str) -> Collection:
+        if self._collections.get(name):
+            return self._collections[name]
+        coll = self.database.get_collection(name)
+        self._collections[name] = coll
+        return coll
+
+    def add_connected_callback(self: Connection,
+                               name: str,
+                               callback: ConnectedCallback) -> None:
+        self._connection_callbacks[name] = callback
+        if self._client:
+            self._call_callback(name, callback)
+
+    def _call_callback(self: Connection,
+                       name: str,
+                       callback: ConnectedCallback) -> None:
+        callback(self.collection(name))
+
+    default: ClassVar[Connection]
+
+
+Connection.default = Connection('default')
