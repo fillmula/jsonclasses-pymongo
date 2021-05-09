58c4d98bb70c68c4a7907d3756194c8e6414b4e7
Victor Zhang <yeannylam@gmail.com>
2021-05-09 11:57:18 +0800

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='jsonclasses-pymongo',
-      version='2.2.6',
+      version='2.3.0',
       description='jsonclasses integration with pymongo',
       long_description=README,
       long_description_content_type="text/markdown",
